3ef68d1812ada626250be54dbcee92fc1f73ef35
Ismael Medina <ismaelmedinasuarez@gmail.com>
2021-11-22 10:15:11 +0100

    Ci?

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,8 @@ using Test
 using TestSetExtensions
 import Pkg
 
+push!(LOAD_PATH,"../")
+
 # Install MultiScaleOT (for CI)
 # Pkg.add(Pkg.PackageSpec(url="https://github.com/ismedina/MultiScaleOT.jl"))
 
