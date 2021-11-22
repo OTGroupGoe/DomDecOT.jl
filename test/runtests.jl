f79266301ca059724daf5d0eb137b5b2b01348a6
Ismael Medina <ismaelmedinasuarez@gmail.com>
2021-11-22 10:05:14 +0100

    Setting up CI: a new hope

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,9 @@
 using Test
 using TestSetExtensions
+import Pkg
+
+# Install MultiScaleOT (for CI)
+Pkg.add(Pkg.PackageSpec(url="https://github.com/ismedina/MultiScaleOT.jl"))
 
 @testset "All the tests" begin
     @includetests ARGS
