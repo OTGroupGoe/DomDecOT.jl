b50c1a867ca778a7c16ef1ffb95fe7ad40365d37
Ismael Medina <ismaelmedinasuarez@gmail.com>
2021-11-22 10:25:05 +0100

    Removed debris, now CI should work

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,5 @@
 using Test
 using TestSetExtensions
-import Pkg
-
-push!(LOAD_PATH,"../")
-
-# Install MultiScaleOT (for CI)
-# Pkg.add(Pkg.PackageSpec(url="https://github.com/ismedina/MultiScaleOT.jl"))
 
 @testset "All the tests" begin
     @includetests ARGS
